158489e23c1a60a6ffdc7d1523f7cd77ed4b80b2
fyuo863 <2357645321@qq.com>
2026-05-07 12:25:41 +0800

    ✨ feat: 添加日志文件轮换功能，加锁

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"go-net-log/internal/fetcher"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -21,23 +22,55 @@ type LogEntry struct {
 
 // Logger 结构体，持有文件句柄
 type Logger struct {
+	mu   sync.Mutex
 	file *os.File
 }
 
-// NewLogger 初始化日志文件
+func nextMidnight() time.Time {
+	now := time.Now()
+	return time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location())
+}
+
+// OpenLogger 初始化日志文件，并启动零点切换协程
 func OpenLogger() *Logger {
+	l := &Logger{}
+	l.openFile()
+
+	go l.rotateAtMidnight()
+
+	return l
+}
+
+func (l *Logger) openFile() {
 	now := time.Now()
 	filename := fmt.Sprintf("network_diag_%s.jsonl", now.Format("2006-01-02"))
 	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		panic(err)
 	}
+	l.file = f
+}
 
-	return &Logger{file: f}
+func (l *Logger) rotateAtMidnight() {
+	timer := time.NewTimer(time.Until(nextMidnight()))
+	defer timer.Stop()
+
+	for {
+		<-timer.C
+
+		l.mu.Lock()
+		l.file.Close()
+		l.openFile()
+		l.mu.Unlock()
+
+		timer.Reset(time.Until(nextMidnight()))
+	}
 }
 
 // Close 关闭文件
 func (l *Logger) Close() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
 	return l.file.Close()
 }
 
@@ -58,6 +91,8 @@ func (l *Logger) Record(d, u float64, gw, pub fetcher.PingResult) error {
 		return err
 	}
 
+	l.mu.Lock()
+	defer l.mu.Unlock()
 	_, err = l.file.Write(append(jsonData, '\n'))
 	return err
 }
